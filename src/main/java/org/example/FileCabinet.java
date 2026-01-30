15695fc3265e69952f886cff21bcebfb5c25dbbb
BogdanMikhalkiv <bogdan.mikhalkiv1@gmail.com>
2026-01-30 12:57:55 +0100

    implemented FileCabinet.findFolderByName and FileCabinet.findFoldersBySize

--- src/main/java/org/example/FileCabinet.java
+++ src/main/java/org/example/FileCabinet.java
@@ -3,16 +3,39 @@ package org.example;
 import java.util.List;
 import java.util.Optional;
 
-public class FileCabinet  implements Cabinet{
+public class FileCabinet implements Cabinet {
     private List<Folder> folders;
+
+    public FileCabinet(List<Folder> folders) {
+        this.folders = folders;
+    }
+
+    public FileCabinet(){
+
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
+
     @Override
     public Optional<Folder> findFolderByName(String name) {
-        return null;
+        Optional<Folder> folderOptional = Optional.ofNullable(
+                folders.stream()
+                        .filter(f -> f.getName().equals(name)).findFirst().orElse(null));
+        return folderOptional;
     }
 
     @Override
     public List<Folder> findFoldersBySize(String size) {
-        return null;
+        List<Folder> folderOptional =
+                folders.stream()
+                        .filter(f -> f.getSize().equals(size)).toList();
+        return folderOptional;
     }
 
     @Override
